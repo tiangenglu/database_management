5695ed7a74dcfcfd027c3fdf8be8a8bc55a8cf4f
tiangenglu <58899759+tiangenglu@users.noreply.github.com>
2025-03-08 00:09:07 -0500

    add sep 2024 opm data

--- 2025-03-07_create_table.sql
+++ 2025-03-07_create_table.sql
@@ -1,5 +1,5 @@
 /* Head count and % by tenure group in cm63 MD HQ */
-CREATE TABLE cm63_hq_mar2024_toa AS
+CREATE TABLE cm63_hq_sep2024_toa AS
 SELECT TOAT AS "tenure-type", COUNT(TOA) AS HEADCOUNT,
 ROUND(100*CAST(COUNT(TOA) AS NUMERIC)/(SELECT COUNT(*) FROM FACT WHERE AGYSUB = 'CM63' AND loc = '24'),2) AS PCT
 FROM FACT
@@ -8,9 +8,9 @@ WHERE AGYSUB = 'CM63' AND loc = '24'
 GROUP BY TOAT
 ORDER BY TOAT, HEADCOUNT;
 /*Add column*/
-ALTER TABLE cm63_hq_mar2024_toa ADD COLUMN time DATE;
+ALTER TABLE cm63_hq_sep2024_toa ADD COLUMN time DATE;
 /*set column value*/
-UPDATE cm63_hq_mar2024_toa SET time = '2024-03-01';
+UPDATE cm63_hq_sep2024_toa SET time = '2024-09-01';
 /*export*/
-COPY cm63_hq_mar2024_toa TO '/Users/tiangeng/Public/data/cm63_hq_mar2024_toa.csv'
+COPY cm63_hq_sep2024_toa TO '/Users/tiangeng/Public/data/cm63_hq_sep2024_toa.csv'
 DELIMITER ',' CSV HEADER;
